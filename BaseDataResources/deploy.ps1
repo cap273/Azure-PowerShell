c102ed2063d3f95f315c73ea1f7b575134f18b48
Carlos Patiño <carlos.patino@catapultsystems.com>
2020-05-14 18:20:23 -0400

    ARM template to deploy ADF, ADLS, and SQL MI

---
+++ BaseDataResources/deploy.ps1
@@ -0,0 +1,26 @@
+﻿$rgName='RG-test08'
+$location= "eastus2"
+
+New-AzResourceGroup -Name $rgName -Location $location
+
+New-AzResourceGroupDeployment -ResourceGroupName $rgName `
+                              -TemplateFile .\azuredeploy.json `
+                              -TemplateParameterFile .\azuredeploy-param.json `
+                              -dataFactoryName "carlosdatafactory4729" `
+                              -storageAccountDataLakeGen2Name "carlosstorageacc4729" `
+                              -sqlManagedInstanceName "carlosmanagedinstance4729" `
+                              -sqlManagedInstanceAdminLogin 'charliebrown' `
+                              -sqlManagedInstancePassword (ConvertTo-SecureString "STRONGPASSWORDHERE" -AsPlainText -Force)
+
+<#
+Note that sqlManagedInstancePassword:
+- must be at least 16 characters in length
+- must be no more than 128 characters in length
+- must contain characters from three of the following categories:
+-- English uppercase letters,
+-- English lowercase letters,
+-- numbers (0-9),
+-- non-alphanumeric characters (!,$,#,%,etc.)
+- cannot contain all of part of the login name. Part of a login name is defined as three
+or more consecutive alphanumeric characters
+#>
